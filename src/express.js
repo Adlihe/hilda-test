88f442df3449b165aa78d1e3c4a4f6420aada29b
Mikael Roos <mikael.t.h.roos@gmail.com>
2025-11-21 15:58:43 +0100

    Done week 02

---
+++ src/express.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import logger from 'morgan'
+import path from 'path'
+import { router } from './route.js'
+
+export const app = express()
+
+// Use a logger
+app.use(logger('dev'))
+
+// Use static content from public/
+app.use(express.static('public'))
+
+// Add EJS template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join('src', 'views'))
+
+// Add the router
+app.use('/', router)
+
+// Error handler for 404
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// Global errorhandler
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  console.error(err)
+  res.status(status).json({
+    status,
+    message: err.message
+  })
+})
---
+++ src/route.js
@@ -0,0 +1,40 @@
+import express from 'express'
+
+export const router = express.Router()
+
+// Hello world route
+router.get('/', (req, res) => {
+  res.send('Hello World! AGAIN SAYS MUMINTROLLET')
+})
+
+// JSON Hello World!
+router.get('/api/hello', (req, res) => {
+    res.json({
+        message: 'Hello world in JSON',
+        now: new Date().toLocaleString('sv-SE', { timeZone: 'Europe/Stockholm'}),
+        random: Math.floor(Math.random() * 100 + 1)
+    })
+})
+
+// A route throwing exceptions
+router.get('/api/error', (req, res) => {
+  throw new Error('This is a forced error for demonstration purposes')  
+})
+
+// A route being forbidden, using exceptions to show it
+router.get('/api/forbidden', (req, res, next) => {
+  const err = new Error('You are not allowed to access this resource')
+  err.status = 403
+  throw err
+})
+
+// A route rendering a page using EJS template/view engine
+router.get('/home', (req, res) => {
+    const data = {
+        header: 'EJS Example',
+        message: 'Hello world in EJS',
+        now: new Date().toLocaleString('sv-SE', { timeZone: 'Europe/Stockholm'}),
+        random: Math.floor(Math.random() * 100 + 1)
+    }
+    res.render('home', data)
+})
